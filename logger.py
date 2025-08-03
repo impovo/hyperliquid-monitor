211ff3d1e4af8a02319a61cf2d27f36dcaa9db40
tootu <impovo@163.com>
2025-08-03 14:48:01 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,3 +3,11 @@
 
 ## 3. 📦 requirements.txt
 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger("hl_monitor")
